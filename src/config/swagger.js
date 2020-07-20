3d8a0bd98b9ecac85832e398b3ab2fcaa69bbfd2
tarang75490 <2018383@iiitdmj.ac.in>
2020-07-20 22:41:14 +0530

    Change for swagger integration in heroku

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -14,8 +14,8 @@ exports.options = {
       url: 'https://swagger.io',
       description: 'Find more info here'
     },
-    host: 'https://colossalproduct.herokuapp.com/',
-    schemes: ['http'],
+    host: 'colossalproduct.herokuapp.com',
+    schemes: ['https'],
     consumes: ['application/json'],
     produces: ['application/json']
   }

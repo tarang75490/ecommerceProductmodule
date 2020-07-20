56eb290ae36272a7d9bf9d62361d2446b23e6ee7
tarang75490 <2018383@iiitdmj.ac.in>
2020-07-20 23:04:45 +0530

    port changed

--- src/app.js
+++ src/app.js
@@ -102,6 +102,6 @@ fastify.listen(PORT, function (err, address) {
     process.exit(1)
   } else {
     // fastify.swagger()
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    fastify.log.info(`server listening on `+PORT)
   }
 });
